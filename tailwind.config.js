efe666383f08c166bdec60b1fc22234d6740e3a2
Michael Dawson <mike@galahadcreative.com>
2023-12-28 14:49:28 +0000

    remove gitignore files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,12 @@ module.exports = {
   darkMode: 'class',
   mode: 'jit',
   content: [
-      './*.html'
+      './*.html',
+      './blog/*.html'
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
 
